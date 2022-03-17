4b8d87b469698f86de33794494a981b2f5a24add
Mikhail <manylattice7742@yandex.kz>
2022-03-17 10:32:09 +0600

    Add Template

---
+++ Templates/File Templates/Cleans Swift MacOS/MacOS.xctemplate/NSViewController/___FILEBASENAME___Worker.swift
@@ -0,0 +1,17 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+
+import Cocoa
+
+class ___VARIABLE_sceneName___Worker {
+    
+    func doSomeWork() {
+    }
+    
+}
